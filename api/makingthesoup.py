e55c8b5af77a1894c43ab62e96af42e97d854af9
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-12-07 19:17:37 +0000

    save soup in file

--- api/makingthesoup.py
+++ api/makingthesoup.py
@@ -8,9 +8,12 @@ r = requests.get('https://www.leagueoflegends.com/en-us/news/game-updates/patch-
 # check status code for response received
 # success code - 200
 print(r)
-print(r.text)
+
 # Parsing the HTML
+
 soup = BeautifulSoup(r.content, 'html.parser')
-# print(soup)
 s = soup.find('div', id='patch-notes-container').get_text("|", strip=True)
-# print(s)
+
+# Save soup in file
+with open("output.txt", "w", encoding="utf-8") as file:
+    file.write(s)
