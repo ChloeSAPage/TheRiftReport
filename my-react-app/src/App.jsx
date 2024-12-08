172f2515a4819b4ae94a57c86ccc976366345f33
Harry Wang <harry.h.wang@outlook.com>
2024-12-08 13:21:48 +0000

    Some design changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,31 +1,46 @@
 import './App.css'
 import lolBg from './assets/lol-bg.jpg'
+import logo from './assets/rift-report-logo.png'
 
 function App() {
-
   return (
-      <section>
-        <div className="nav-bar font-inter font-medium">
-          <p className="container mx-auto">
-            The Rift Report
+    <section>
+      <div className="nav-bar h-16 flex items-center">
+        <img src={logo} className="h-full object-contain ml-2"></img>
+        <p className="container mx-auto font-futura font-medium bg-black my-3 ml-3">
+          The Rift Report
+        </p>
+      </div>
+      <div className="w-full h-[300px] relative">
+        <img src={lolBg} alt="banner" className="w-full h-full object-cover brightness-50"/>
+        <div className="absolute px-32 flex top-0 bottom-0 items-center justify-center w-full flex-col">
+          <img src={logo} className="h-[150px]"></img>
+        </div>
+      </div>
+      <div className="px-32">
+        <div className="container mx-auto p-5 bg-white">
+          <h1 className='font-bold text-xl font-futura w-3/5 mb-4'>
+            Latest Patch Summary:
+          </h1>
+          <p>
+            The latest patch has brought a lot of changes to the game. 
+            <br></br>
+            The meta has shifted and new champions are now in the spotlight.
+            <br></br> 
+            Here are some of the highlights:
+            <br></br>
+            Ivern is in the dirt
+            <br></br>
+            Azir is still in the dirt
+            <br></br>
+            No one saw this coming
+            <br></br>
+            Woah
           </p>
         </div>
-        <div className="container mx-auto my-5 px-64">
-          <div>
-            <div>
-              <h1 className='font-bold text-2xl font-inter w-2/3'>Wondering about the newest League patch?</h1>
-            </div>
-            <div className="flex justify-end">
-              <h1 className='font-bold text-2xl font-inter w-2/3 text-right'>
-              Or what is trending on X? <br></br>Or Reddit?</h1>
-            </div>
-            <div className="flex justify-center mt-3">
-              <img src={lolBg} alt="placeholder" className="w-full h-72 object-cover" />
-            </div>
-          </div>
-        </div>
-      </section>
+      </div>
+    </section>
   )
 }
 
-export default App
+export default App;
